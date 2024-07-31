f3d9535a7cb0a0ca48663233c64348690aa55e9b
Umar <umardxbfarooq@gmail.com>
2024-07-30 23:38:18 -0400

    third commit

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -2,7 +2,7 @@ export default function AboutMe() {
     return (
         <div className="about-me">
             <h1>About Me</h1>
-            <img className = "pfp" src = 'public/assets/square_pfp.png' alt = "Profile Picture"></img>
+            <img className = "pfp" src = '/assets/square_pfp.png' alt = "Profile Picture"></img>
             <p className="about-me-description">
                 I'm Umar, a 17 year old with a passion for grand strategy games and trains.
             </p>
